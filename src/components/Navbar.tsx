e949a029653928d821dc3d9398a175c4a30b6216
[dyad] <Ggattone@outlook.com>
2025-09-07 10:31:43 +0200

    [dyad] Correzione del ritaglio del logo - wrote 3 file(s)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -101,7 +101,7 @@ export const Navbar = () => {
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="font-bold text-xl text-primary">
             {theme?.logo_url ? (
-              <img src={getOptimizedImageUrl(theme.logo_url, { height: 40 })} alt="Logo" className="h-10 max-w-xs" />
+              <img src={getOptimizedImageUrl(theme.logo_url, { height: 40 })} alt="Logo" className="h-10 max-w-xs object-contain" />
             ) : (
               <span>NC League</span>
             )}
